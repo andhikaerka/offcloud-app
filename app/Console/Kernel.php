c34b034b68d2dbcd79de209b61f9b1b637bcaedc
erkalodon <andhika.ragilkesuma@gmail.com>
2021-03-06 09:50:52 +0700

    Update torrent:send

--- app/Console/Kernel.php
+++ app/Console/Kernel.php
@@ -26,7 +26,7 @@ class Kernel extends ConsoleKernel
     {
         // $schedule->command('inspire')->hourly();
 
-        $schedule->command('torrent:send')->everyFiveMinutes();
+        $schedule->command('torrent:send')->everyMinute();
     }
 
     /**
